d473b9f38a54fdf5180ac0449e0894a773c001e4
maxik555 <75203466+maxik555@users.noreply.github.com>
2026-03-09 20:23:58 +0100

    Update sensei.js

--- js/sensei.js
+++ js/sensei.js
@@ -12,7 +12,7 @@ async function callGemini(promptText) {
     }
 
     try {
-        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${key}`, {
+        const response = await fetch(`https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${key}`,  {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ contents: [{ parts: [{ text: promptText }] }] })
